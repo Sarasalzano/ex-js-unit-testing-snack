b12a7d4dbf3babca3f3c494055cf3bc074449352
Sara Salzano <sarasalzano95@gmail.com>
2026-01-12 15:45:49 +0100

    esegui funzione corretta snack 3

--- snack3.test.js
+++ snack3.test.js
@@ -1,4 +1,10 @@
-test("La funzione average calcola la media aritmetica di un array di numeri.", ()=> {
-    const media = average([4, 10]);
-    expect(media).toBe(7)
+function average(array) {
+  const sum = array.reduce((acc, num) => acc + num, 0);
+  const averageNum = (sum / array.length);
+  return averageNum
+}
+
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+  const media = average([4, 10]);
+  expect(media).toBe(7);
 });
