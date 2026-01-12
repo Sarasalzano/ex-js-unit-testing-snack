f43db585f7305bdc96a0e1cf5f720ac6166d79e0
Sara Salzano <sarasalzano95@gmail.com>
2026-01-12 17:07:06 +0100

    esegui funzione corretta snack 7

--- snack7.test.js
+++ snack7.test.js
@@ -1,14 +1,13 @@
-// Creare un test che verifichi le seguenti descrizioni:
+const posts = [
+  { id: 1, title: "Primo post", slug: "primo-post" },
+  { id: 2, title: "Secondo post", slug: "secondo-post" },
+  { id: 3, title: "Terzo post", slug: "terzo-post" },
+];
 
-// "La funzione findPostById restituisce il post corretto dato l’array di post e l’id"
-
-// Creare uno o più test aggiuntivi che controllino che la struttura dati passati sia conforme (ogni post ha le proprietà id, title e slug, viene passato un id numerico).
+function findPostById(posts, id) {
+  return posts.find((post) => post.id === id);
+}
 
 test("La funzione findPostById restituisce il post corretto dato l’array di post e l’id", () => {
-  const posts = [
-    { id: 1, title: "Primo post", slug: "primo-post" },
-    { id: 2, title: "Secondo post", slug: "secondo-post" },
-    { id: 3, title: "Terzo post", slug: "terzo-post" },
-  ];
   expect(findPostById(posts, 1)).toEqual(posts[0]);
 });
