f1646c59d66ca5a202398827cb6644d954f8f946
Sara Salzano <sarasalzano95@gmail.com>
2026-01-12 15:31:54 +0100

    esegui funzione corretta snack2

--- snack2.test.js
+++ snack2.test.js
@@ -1,3 +1,8 @@
+function createSlug(slug){
+    const lowerCase = slug.toLowerCase();
+    return lowerCase
+}
+
 
 test("La funzione createSlug restituisce una stringa in lowercase.", () => {
     const slug = createSlug("Hello World")
