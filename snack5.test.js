2404a437bee0d3931dd42764b09a7ae591470728
Sara Salzano <sarasalzano95@gmail.com>
2026-01-12 16:24:42 +0100

    esegui funzione corretta snack5

--- snack5.test.js
+++ snack5.test.js
@@ -1,4 +1,16 @@
+function isPalindrome(word) {
+  let reversed = "";
+  for (let i = word.length -1; i >= 0; i--) {
+    reversed += word[i];
+  }
+  if (reversed === word) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
-const palindromeWord = isPalindrome("ingegni");
-expect(palindromeWord).toBe(true)
+  const palindromeWord = isPalindrome("ingegni");
+  expect(palindromeWord).toBe(true);
 });
